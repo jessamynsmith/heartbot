17b28680c466cb00e20da586f25ec36ba60b16dc
jessamynsmith <jessamyn.smith@gmail.com>
2019-05-29 13:50:48 -0600

    Added deployment script needed for new circleci config. Updated Heroku stack to 18 and python to 3.7.

---
+++ scripts/deploy_to_heroku.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script will quit on the first error that is encountered.
+set -e
+
+CIRCLE=$1
+
+if [ $CIRCLE ]
+then
+    git push https://heroku:$HEROKU_API_KEY@git.heroku.com/heartbot.git master
+else
+    git push heroku master
+fi
